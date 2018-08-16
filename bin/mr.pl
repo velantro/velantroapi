756e197851bddb8893913c4a06ddd0b97e3fd74e
salzh <zhongxiang721@gmail.com>
2018-08-16 20:05:09 +0800

    Add mr.pl into repos

---
+++ bin/mr.pl
@@ -0,0 +1,27 @@
+
+$s = '/usr/local/freeswitch/recordings/';
+if (not -d $s) {
+   $s = "/var/lib/freeswitch/recordings/";
+}
+
+if (!-e "/mnt/s3/iams3") {
+    die "not found /mnt/s3/iams3";
+}
+for $f (split /\n/, `find $s -mtime +7 -name "*.wav"  | grep  'archive'`) {
+        warn "mv $f\n";
+	next if -l $f;
+        ($dir, $n) = $f =~ /(.+)\/(.+\.wav)$/;
+
+        $destdir = "/mnt/s3$dir";
+        if (!-d $destdir) {
+            system("mkdir -p $destdir");
+
+        }
+
+        warn "copy $f to $destdir\n";
+        system("mv $f $destdir");
+
+        warn "ln -s $destdir/$n $f";
+        system("ln -s $destdir/$n $f");
+
+    }
