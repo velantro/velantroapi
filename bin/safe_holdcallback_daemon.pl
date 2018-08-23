2515ba709f26ac3eb33a5c31ec4e3d5ecfab43e1
salzh <zhongxiang721@gmail.com>
2018-08-23 10:12:45 +0800

    update codes

--- bin/safe_holdcallback_daemon.pl
+++ bin/safe_holdcallback_daemon.pl
@@ -2,7 +2,7 @@
 $daemon_script = "/salzh/velantroapi/bin/app_konference.listener.pl";
 	
 while (1) {
-	system("perl $daemon_script");
+	system("perl $daemon_script > /tmp/h.log 2>&1");
 	warn "\n\n!!!!!!!!!!!!!!!!!!!\n convert_daemon stop,let's restart it\n\n";
 	
 	sleep 2;
