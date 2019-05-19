c36164ead6f11be05141094d70024b434eb70cb2
salzh <zhongxiang721@gmail.com>
2019-05-19 08:25:07 +0800

    update

--- bin/delete_cdr.sh
+++ bin/delete_cdr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "delete  from v_xml_cdr where  start_stamp < CURRENT_DATE - INTERVAL '6 months' order by start_stamp desc limit 10;" | psql fusionpbx -U fusionpbx -h 127.0.0.1
+echo "delete  from v_xml_cdr where  start_stamp < CURRENT_DATE - INTERVAL '6 months'" | psql fusionpbx -U fusionpbx -h 127.0.0.1
