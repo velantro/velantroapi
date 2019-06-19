425993662357bfc34313db1257ffef36408bc693
salzh <zhongxiang721@gmail.com>
2019-06-19 20:30:42 +0800

    Add test_api_sendcallback.pl to test sendcallback api on different server

---
+++ bin/test_api_sendcallback.pl
@@ -0,0 +1,12 @@
+$tenant = shift;
+$ext = shift;
+$dest = shift;
+$port = shift || 8080;
+
+$cookie = "/tmp/velantroapi.txt";
+$cmd = "curl -k  -s  -c $cookie \"http://$tenant.velantro.net:$port/api/api2.pl?action=login&&username=api&&password=api123@@\"";
+warn $cmd, "\n";
+system($cmd);
+$cmd = "curl -k  -s  -c $cookie \"http://$tenant.velantro.net:$port/api/api2.pl?action=sendcallback&&ext=$ext&&dest=$dest\"";
+warn $cmd, "\n";
+system($cmd);
