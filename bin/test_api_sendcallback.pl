1980f34ad8b5c30a5eb08f5e4bcaa7323743bc53
salzh <zhongxiang721@gmail.com>
2019-07-03 20:52:25 +0800

    Add autoanswer=1 in test sendcallback script

--- bin/test_api_sendcallback.pl
+++ bin/test_api_sendcallback.pl
@@ -7,6 +7,6 @@ $cookie = "/tmp/velantroapi.txt";
 $cmd = "curl -k  -s  -c $cookie \"http://$tenant.velantro.net:$port/api/api2.pl?action=login&&username=api&&password=api123@@\"";
 warn $cmd, "\n";
 system($cmd);
-$cmd = "curl -k  -s  -b $cookie \"http://$tenant.velantro.net:$port/api/api2.pl?action=sendcallback&&ext=$ext&&dest=$dest\"";
+$cmd = "curl -k  -s  -b $cookie \"http://$tenant.velantro.net:$port/api/api2.pl?action=sendcallback&&ext=$ext&&dest=$dest&autoanswer=1\"";
 warn $cmd, "\n";
 system($cmd);
