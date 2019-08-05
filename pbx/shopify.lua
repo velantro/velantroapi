5032e5327e5e0e406a6ddee80ca14f3d7d0954c0
salzh <zhongxiang721@gmail.com>
2019-08-05 10:41:54 +0800

    Add shoify.lua and lua library scripts

---
+++ pbx/shopify.lua
@@ -0,0 +1,22 @@
+debug["info"] = true;
+local json
+if (debug["sql"]) then
+    json = require "resources.functions.lunajson"
+end
+
+
+cmd =" curl -k 'https://15a24d3cd32140671569ec08b1c24e58:726d5b90f6fb1a6072d8a032555c7cbe@velantrodev.myshopify.com/admin/api/2019-07/customers/search.json?query=phone:7474779513&fields=first_name,last_name'";
+if (debug["info"]) then
+    freeswitch.consoleLog("notice", "[sms] CMD: " .. cmd .. "\n");
+end
+local handle = io.popen(cmd)
+local result = handle:read("*a")
+handle:close()
+if (debug["info"]) then
+    freeswitch.consoleLog("notice", "[sms] CURL Returns: " .. result .. "\n");
+end
+
+response = json.decode(result);
+
+freeswitch.consoleLog("notice", "json response" .. json.encode(response) .. "\n");
+
