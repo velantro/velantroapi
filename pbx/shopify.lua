0f8b76ea9792082ce20b4de740dbf3cc228ba777
salzh <zhongxiang721@gmail.com>
2019-08-05 12:45:05 +0800

    update

--- pbx/shopify.lua
+++ pbx/shopify.lua
@@ -7,15 +7,15 @@ end
 
 
 function request(path)
-    local cmd = "curl -k 'https://15a24d3cd32140671569ec08b1c24e58:726d5b90f6fb1a6072d8a032555c7cbe@velantrodev.myshopify.com" .. path;
+    local cmd = "curl -k 'https://15a24d3cd32140671569ec08b1c24e58:726d5b90f6fb1a6072d8a032555c7cbe@velantrodev.myshopify.com" .. path . "'";
     if (debug["info"]) then
-        freeswitch.consoleLog("notice", "[sms] CMD: " .. cmd .. "\n");
+        freeswitch.consoleLog("notice", "[shopify.lua] CMD: " .. cmd .. "\n");
     end
     local handle = io.popen(cmd)
     local result = handle:read("*a")
     handle:close()
     if (debug["info"]) then
-        freeswitch.consoleLog("notice", "[sms] CURL Returns: " .. result .. "\n");
+        freeswitch.consoleLog("notice", "[shopify.lua] CURL Returns: " .. result .. "\n");
     end
     
     local response = json.decode(result);
