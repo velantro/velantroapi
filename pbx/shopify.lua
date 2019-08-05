9d9be3a7ee1a99ed20bc0795e16c8c249d8a25ef
salzh <zhongxiang721@gmail.com>
2019-08-05 12:24:29 +0800

    write full ivr for shopify

--- pbx/shopify.lua
+++ pbx/shopify.lua
@@ -5,19 +5,10 @@ if (debug["info"]) then
 end
 
 
-cmd =" curl -k 'https://15a24d3cd32140671569ec08b1c24e58:726d5b90f6fb1a6072d8a032555c7cbe@velantrodev.myshopify.com/admin/api/2019-07/customers/search.json?query=phone:18185784000&fields=first_name,last_name'";
-if (debug["info"]) then
-    freeswitch.consoleLog("notice", "[sms] CMD: " .. cmd .. "\n");
-end
-local handle = io.popen(cmd)
-local result = handle:read("*a")
-handle:close()
-if (debug["info"]) then
-    freeswitch.consoleLog("notice", "[sms] CURL Returns: " .. result .. "\n");
-end
 
-response = json.decode(result);
+local response = request("/admin/api/2019-07/customers/search.json?query=phone:18185784000&fields=first_name,last_name");
 
+customer_id = response["customers"][1]["id"];
 --freeswitch.consoleLog("notice", "first_name:" .. response["customers"][1]["first_name"] .. " last_name:" .. response["customers"][1]["last_name"]);
 shop = "velantro"
 if ( session:ready() ) then
@@ -31,4 +22,55 @@ if ( session:ready() ) then
     filename = api:execute("system", "php /var/www/api/pbx/polly_bin.php '" .. tts_text .. "'");
     
     session:streamFile(filename);
+    
+    
+    min_digits=1;
+    max_digits=1;
+    max_tries = 2;
+    digit_timeout=15000;
+    
+     
+    dtmf = session:playAndGetDigits(min_digits, max_digits, max_tries, digit_timeout, "#", filename, "", "\\d");
+    if (not customer_id) then
+        dtmf = '2';
+    end
+    if (dtmf == '1') then
+        local response = request("/admin/api/2019-07/customers/" .. customer_id .. ".json');
+        last_order_id = response["customer"]["last_order_id"];
+        if (not last_order_id) then
+            filename = dotts("Your last order was on" .. response["order"]["created_at"].." if yes press 1 if no press 2.")
+            dtmf = session:playAndGetDigits(min_digits, max_digits, max_tries, digit_timeout, "#", filename, "", "\\d");
+            if (dtmf == '1') then
+                status = response["order"]["financial_status"];
+                filename = dotts("Your order status is " .. status ..
+                                 "  the shipping carrier is " .. "it will arrive on  " )
+                session:streamFile(filename);
+                
+                filename = dotts("Press 5 to email you the tracking number of your shipment." .. "To return to main menu press *");
+                session:streamFile(filename);
+
+            end
+        end
+    end
+end
+
+function request(path)
+    local cmd = "curl -k 'https://15a24d3cd32140671569ec08b1c24e58:726d5b90f6fb1a6072d8a032555c7cbe@velantrodev.myshopify.com" .. path;
+    if (debug["info"]) then
+        freeswitch.consoleLog("notice", "[sms] CMD: " .. cmd .. "\n");
+    end
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    handle:close()
+    if (debug["info"]) then
+        freeswitch.consoleLog("notice", "[sms] CURL Returns: " .. result .. "\n");
+    end
+    
+    local response = json.decode(result);
+    return response;
+end
+
+function dotts(tts_text)
+    filename = api:execute("system", "php /var/www/api/pbx/polly_bin.php '" .. tts_text .. "'");
+    return filename;
 end
