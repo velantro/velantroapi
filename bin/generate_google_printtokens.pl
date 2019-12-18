b821a12e873a82e78d183c2ca37314c8b9b10349
salzh <zhongxiang721@gmail.com>
2019-12-18 15:16:49 +0800

    update

--- bin/generate_google_printtokens.pl
+++ bin/generate_google_printtokens.pl
@@ -3,10 +3,11 @@ $output = `gcloud --format=text projects list`;
 for $row (split /\n/, $output) {
    if ($row =~ /projectId:\s+(.+)$/) {
       $agent = $1;
-      warn "Start generate token for $agent: ";
+      warn "Start generate token for $agent: \n";
       
       $token = `gcloud iam service-accounts keys create /tmp/$agent.json --iam-account smalltalkapi2\@$agent.iam.gserviceaccount.com`;
-      print "$token !\n";
+      $token = `export GOOGLE_APPLICATION_CREDENTIALS=/tmp/$agent.json ; gcloud auth application-default print-access-token`;
+      print $token, "\n";
    }
    
 }
