919d90fc69dc5e32af732a980e94e1c68a234548
salzh <zhongxiang721@gmail.com>
2020-01-07 09:07:50 +0800

    Add verbose

--- bin/clear_recordings_s3.sh
+++ bin/clear_recordings_s3.sh
@@ -1,12 +1,12 @@
-rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2014/*
-rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2015/*
-rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2016/*
-rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2017/*
+rm -rf -v /mnt/s3/usr/local/freeswitch/recordings/*/archive/2014/*
+rm -rf -v /mnt/s3/usr/local/freeswitch/recordings/*/archive/2015/*
+rm -rf -v /mnt/s3/usr/local/freeswitch/recordings/*/archive/2016/*
+rm -rf -v /mnt/s3/usr/local/freeswitch/recordings/*/archive/2017/*
 
-rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2014/*
-rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2015/*
-rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2016/*
-rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2017/*
-rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2018/*
+rm -rf -v /mnt/s3/var/lib/freeswitch/recordings/*/archive/2014/*
+rm -rf -v /mnt/s3/var/lib/freeswitch/recordings/*/archive/2015/*
+rm -rf -v /mnt/s3/var/lib/freeswitch/recordings/*/archive/2016/*
+rm -rf -v /mnt/s3/var/lib/freeswitch/recordings/*/archive/2017/*
+rm -rf -v /mnt/s3/var/lib/freeswitch/recordings/*/archive/2018/*
 
 
