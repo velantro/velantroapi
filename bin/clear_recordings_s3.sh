6ba8c82633f2e279c5220b7aee18962e1e48ec54
salzh <zhongxiang721@gmail.com>
2020-01-07 09:07:08 +0800

    Add 2018 directory for recordings saved in var

--- bin/clear_recordings_s3.sh
+++ bin/clear_recordings_s3.sh
@@ -7,5 +7,6 @@ rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2014/*
 rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2015/*
 rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2016/*
 rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2017/*
+rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2018/*
 
 
