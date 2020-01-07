89c34cf392d80a4229d27dedea5a218a3ff8e647
salzh <zhongxiang721@gmail.com>
2020-01-07 09:01:27 +0800

    Add script to delete old recordings on s3. rapidins 2 years. others 1 year

---
+++ bin/clear_recordings_s3.sh
@@ -0,0 +1,11 @@
+rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2014/*
+rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2015/*
+rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2016/*
+rm -rf /mnt/s3/usr/local/freeswitch/recordings/*/archive/2017/*
+
+rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2014/*
+rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2015/*
+rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2016/*
+rm -rf /mnt/s3/var/lib/freeswitch/recordings/*/archive/2017/*
+
+
--- bin/generate_ai_token.pl
+++ bin/generate_ai_token.pl
@@ -1,17 +1,46 @@
-$account = shift;
-$agent = shift;
+use JSON;
+$basedir = "/var/www/google";
+$gcloud_bin = "/var/www/google-cloud-sdk/bin/gcloud";
 
-$user = "smsapi";
-system("CLOUDSDK_CONFIG=/var/www/google/$account  /var/www/google-cloud-sdk/bin/gcloud config set project $agent");
+for $dir (glob("$basedir/*")) {
+    warn $dir;
+    $ac = substr $dir, length($basedir)+1;
+    push @accounts, $ac;
+}
 
-system("CLOUDSDK_CONFIG=/var/www/google/$account  /var/www/google-cloud-sdk/bin/gcloud  iam service-accounts create  $user");
+for $ac (@accounts) {
+    warn "list agents for account $ac!\n";
+    $json = `CLOUDSDK_CONFIG=$basedir/$ac  $gcloud_bin  --format=json projects list`;
+    $hash = decode_json($json);
+    
+    for $ag (@$hash) {
+        warn "agent: " . $ag->{projectId} . "\n";
+        warn "generate print token for $ac - " . $ag->{projectId} . "\n";
+        &generate_print_token($ac, $ag->{projectId});
+    }
+}
 
-system("CLOUDSDK_CONFIG=/var/www/google/$account  /var/www/google-cloud-sdk/bin/gcloud projects add-iam-policy-binding  $agent  --member \"serviceAccount:$user\@$agent.iam.gserviceaccount.com\" --role \"roles/owner\"");
-
-system("CLOUDSDK_CONFIG=/var/www/google/$account  /var/www/google-cloud-sdk/bin/gcloud iam service-accounts keys create /tmp/$agent.json --iam-account $user\@$agent.iam.gserviceaccount.com");
-
-system("export GOOGLE_APPLICATION_CREDENTIALS=/tmp/$agent.json; CLOUDSDK_CONFIG=/var/www/google/$account  /var/www/google-cloud-sdk/bin/gcloud  auth application-default print-access-token > /var/www/google/$account/$agent.conf");
-
-system("cat /var/www/google/$account/$agent.conf");
 
 
+sub generate_print_token {
+    $account = shift;
+    $agent = shift;
+    if (-e "$basedir/$account/$agent.conf") {
+        warn "$basedir/$account/$agent.conf alreay generated!!\n";
+        return;
+    }
+    
+    $user = "smsapi";
+    system("CLOUDSDK_CONFIG=$basedir/$account  $gcloud_bin config set project $agent");
+    
+    system("CLOUDSDK_CONFIG=$basedir/$account  $gcloud_bin  iam service-accounts create  $user");
+    
+    system("CLOUDSDK_CONFIG=$basedir/$account  $gcloud_bin projects add-iam-policy-binding  $agent  --member \"serviceAccount:$user\@$agent.iam.gserviceaccount.com\" --role \"roles/owner\"");
+    
+    system("CLOUDSDK_CONFIG=$basedir/$account  $gcloud_bin iam service-accounts keys create /tmp/$agent.json --iam-account $user\@$agent.iam.gserviceaccount.com");
+    
+    system("export GOOGLE_APPLICATION_CREDENTIALS=/tmp/$agent.json; CLOUDSDK_CONFIG=$basedir/$account  $gcloud_bin  auth application-default print-access-token > $basedir/$account/$agent.conf");
+    
+    system("cat $basedir/$account/$agent.conf");
+
+}
