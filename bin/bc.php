4270240821090d1fb3ffb8f80915a1e1419c3129
salzh <zhongxiang721@gmail.com>
2021-01-10 20:33:50 +0800

    u

--- bin/bc.php
+++ bin/bc.php
@@ -6,14 +6,31 @@ $descriptorspec = array(
 );
 
 $cwd = '/tmp';
-$env = array('some_option' => 'aeiou');
-
+#$env = array('some_option' => 'aeiou');
+/*
 $process = proc_open('php', $descriptorspec, $pipes, $cwd, $env);
 
 if (is_resource($process)) {
     
 
     fwrite($pipes[0], '<?php print_r($_ENV); ?>');
+    fclose($pipes[0]);
+
+    echo stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    
+
+    $return_value = proc_close($process);
+
+    echo "command returned $return_value\n";
+}*/
+
+$process = proc_open('CLOUDSDK_CONFIG=/var/www/zhongxiang721 /var/www/google-cloud-sdk/bin/gcloud auth login', $descriptorspec, $pipes, $cwd);
+
+if (is_resource($process)) {
+    
+
+    #fwrite($pipes[0], '');
     fclose($pipes[0]);
 
     echo stream_get_contents($pipes[1]);
