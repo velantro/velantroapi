c153846f260d2dac0e24665a8be2ddf6c2c6d729
salzh <zhongxiang721@gmail.com>
2021-05-03 12:34:43 +0800

    add updatewasabi2 in acmd

--- bin/acmd.pl
+++ bin/acmd.pl
@@ -1,34 +1,30 @@
 $lines = '
-67.227.80.76,22,velantro76,http://76.velantro.net/
-67.227.80.67,22,velantro67,http://67.227.80.67/
-67.227.80.71,22,velantro71,http://67.227.80.71/
-67.227.80.83,22,velantro204,http://204.velantro.net/
-67.227.80.51,56443,velantro19,http://19.velantro.net/
-67.227.80.35,56443,velantro243,http://243.velantro.net/
-67.227.80.37,56443,velantro245,http://245.velantro.net
-208.76.253.123,56443,velantrovip,http://vip.velantro.net
+67.227.80.76,22,velantro76,http://76.velantro.net/,pbxv1
+67.227.80.67,22,velantro67,http://67.227.80.67/,pbxv1
+67.227.80.71,22,velantro71,http://67.227.80.71/,pbxv1
+67.227.80.83,22,velantro204,http://204.velantro.net/,pbxv1
+67.227.80.51,56443,velantro19,http://19.velantro.net/,pbxv1
+67.227.80.35,56443,velantro243,http://243.velantro.net/,pbxv1
+67.227.80.37,56443,velantro245,http://245.velantro.net,pbxv1
+208.76.253.123,56443,velantrovip,http://vip.velantro.net,pbxv1
 67.207.164.220,56443,velantro220,http://220.velantro.net
-67.227.80.53,22,velantro53,http://67.227.80.53
-67.227.80.55,22,velantro55,http://67.227.80.55/
-67.227.80.56,22,velantro56,http://67.227.80.56
-67.227.80.20,22,velantro20,http://67.227.80.20
-67.227.80.12,22,velantro12,http://67.227.80.12
-67.227.80.6,22,velantro6,http://67.227.80.6
-208.76.253.122,22,velantro122,http://208.76.253.122/
-208.76.253.124,22,velantrogbm,http://gbmllc.velantro.net/
-208.76.253.121,22,velantro121,http://208.76.253.121/
-208.76.253.116,22,velantro116,http://208.76.253.116/
-208.76.253.115,22,velantro115,http://208.76.253.115/
-208.76.253.118,22,velantro118,http://208.76.253.118/
+67.227.80.53,22,velantro53,http://67.227.80.53,pbxv1
+67.227.80.55,22,velantro55,http://67.227.80.55/,pbxv2
+67.227.80.56,22,velantro56,http://67.227.80.56,pbxv2
+67.227.80.20,22,velantro20,http://67.227.80.20,pbxv2
+67.227.80.12,22,velantro12,http://67.227.80.12,pbxv2
+67.227.80.6,22,velantro6,http://67.227.80.6,pbxv2
+208.76.253.122,22,velantro122,http://208.76.253.122/,pbxv2
+208.76.253.124,22,velantrogbm,http://gbmllc.velantro.net/,pbxv2
+208.76.253.121,22,velantro121,http://208.76.253.121/,pbxv2
+208.76.253.116,22,velantro116,http://208.76.253.116/,pbxv2
+208.76.253.115,22,velantro115,http://208.76.253.115/,pbxv2
+208.76.253.118,22,velantro118,http://208.76.253.118/,pbxv2
 208.76.253.119,22,shy,http://ml.managedlogix.net
 67.227.80.11,56443,ml2,http://newmentor.managedlogix.net/
 67.227.80.10,56443,ml3,http://67.227.80.10
-67.227.80.36,56443,liv,http://liv.livvoip.net
-67.227.80.22,22,liv2,http://67.227.80.22/
 67.227.80.21,22,serv1,http://67.227.80.21/
 67.227.80.23,22,serv2,http://67.227.80.23/
-67.227.80.26,22,gosimplevoice,http://http://serv1.gosimplevoice.net/
-67.207.164.219,22,multicomm,http://67.207.164.219/
 ';
 
 $cmd = shift || exit;
@@ -137,12 +133,23 @@ if ($cmd eq 'updatemonitorscript') {
 } elsif ($cmd eq 'updatewasabi') {
 	
 	for (split /\n/, $lines) {
-		($ip,$port,$name,$uri) = split ',', $_, 4;
+		($ip,$port,$name,$uri,$v) = split ',', $_, 5;
 		next if !$ip;
-		next unless $name =~ /velantro/;
+		next unless $v eq 'pbxv1';
 		print "$cmd on  $name [$ip:$port]\n";
 		
 		system("scp -oPort=$port /salzh/bin/mounts3 root\@$ip:/salzh/bin")
-
+		system("scp -oPort=$port /usr/local/bin/s3fsnew root\@$ip:/usr/local/bin/");
+	}
+} elsif ($cmd eq 'updatewasabi2') {
+	
+	for (split /\n/, $lines) {
+		($ip,$port,$name,$uri,$v) = split ',', $_, 5;
+		next if !$ip;
+		next unless $v eq 'pbxv2';
+		print "$cmd on  $name [$ip:$port]\n";
+		
+		system("scp -oPort=$port /salzh/bin/mounts3 root\@$ip:/salzh/bin");
+		system("ssh -t -p $port root\@$ip cp -f /usr/local/bin/s3fs /usr/local/bin/s3fsnew");
 	}
 } 
