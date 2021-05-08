9555496b63d6efffa6166e1066de560844c1c23d
salzh <zhongxiang721@gmail.com>
2021-05-08 14:15:46 +0800

    add script to compare recordings on s3 and wasabi

--- bin/compare_wa_s3.pl
+++ bin/compare_wa_s3.pl
@@ -1,7 +1,10 @@
 @base_dir = ('var/lib/freeswitch', 'usr/local/freeswitch');
+@months = qw/Apr  Aug  Dec  Feb  Jan  Jul  Jun  Mar  May  Nov  Oct  Sep/;
 
 for $b (@base_dir) {
    while (</mnt/s3/$b/recordings/*>) {
-      print $_, "\n";
+      #print $_, "\n";
+      ($d) = $_ =~ m{recordings/(.+)$};
+      print $d, "\n";
    }   
 }
