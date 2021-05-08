9466ce66fe75ad5392e8aa345e2ecb80c0464128
salzh <zhongxiang721@gmail.com>
2021-05-08 21:31:42 +0800

    add script to generate copy recordings commands

--- bin/generate_mv.pl
+++ bin/generate_mv.pl
@@ -1,22 +1,14 @@
 @lines = qw{
-******/mnt/wasabi/var/lib/freeswitch/recordings/getrightlogistics.velantro.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/helpinghands2048w238.nexlevel2.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/krftdsushi.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/larisins.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/legalservice.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/level2security.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/mmcarstruckvans.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/mprfunding.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/navarettefinancial.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/nexera.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/plesrv1.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/royalprestige.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/sdas.nexeravoice.net
-******/mnt/wasabi/var/lib/freeswitch/recordings/solarclean.nexeravoice.net
+*****/mnt/wasabi/var/lib/freeswitch/recordings/rlgstaterealty.velantro.net/archive/2021
+*****/mnt/wasabi/usr/local/freeswitch/recordings/jlpdispatching.velantro.net/archive/2020
+*****/mnt/wasabi/usr/local/freeswitch/recordings/judicialguardllp.velantro.net/archive/2020
+*****/mnt/wasabi/usr/local/freeswitch/recordings/juicywingzinc.velantro.net/archive/2020
+*****/mnt/wasabi/usr/local/freeswitch/recordings/justelitetransport.velantro.net/archive/2020
 };
 
+print "#!/bin/sh\n";
 for $e (@lines) {
-   ($d) = $e =~ m{\*\*\*\*\*\*/mnt/wasabi/var/lib/freeswitch/recordings/(.+)$};
-   $cmd = "setsid cp -purfn /mnt/s3/var/lib/freeswitch/recordings/$d /mnt/wasabi/var/lib/freeswitch/recordings";
+   ($p, $d, $y) = $e =~ m{\*\*\*\*\*/mnt/wasabi/(.+?)/freeswitch/recordings/(.+)/archive/(\d+)$};
+   $cmd = "cp -purfn /mnt/s3/$p/freeswitch/recordings/$d/archive/$y /mnt/wasabi/$p/freeswitch/recordings/$d/archive &";
    print $cmd, "\n";
 }
