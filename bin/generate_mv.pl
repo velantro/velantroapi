3a6046172de565389b82b6941a5285d6ea8c99b2
salzh <zhongxiang721@gmail.com>
2021-05-08 21:15:19 +0800

    add script to generate copy recordings commands

---
+++ bin/generate_mv.pl
@@ -0,0 +1,22 @@
+@lines = qw{
+******/mnt/wasabi/var/lib/freeswitch/recordings/getrightlogistics.velantro.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/helpinghands2048w238.nexlevel2.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/krftdsushi.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/larisins.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/legalservice.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/level2security.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/mmcarstruckvans.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/mprfunding.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/navarettefinancial.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/nexera.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/plesrv1.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/royalprestige.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/sdas.nexeravoice.net not found
+******/mnt/wasabi/var/lib/freeswitch/recordings/solarclean.nexeravoice.net not found
+};
+
+for $e (@lines) {
+   ($d) = $e =~ m{\*\*\*\*\*\*/mnt/wasabi/var/lib/freeswitch/recordings/(.+)? not found};
+   $cmd = "setsid cp -purfn /mnt/s3/var/lib/freeswitch/recordings/$d /mnt/wasabi/var/lib/freeswitch/recordings";
+   print $cmd, "\n";
+}
