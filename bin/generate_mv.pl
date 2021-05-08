7f94cbed6915182922609685e51a602214d46c3b
salzh <zhongxiang721@gmail.com>
2021-05-08 21:38:21 +0800

    add script to generate copy recordings commands

--- bin/generate_mv.pl
+++ bin/generate_mv.pl
@@ -1,14 +1,40 @@
 @lines = qw{
-*****/mnt/wasabi/var/lib/freeswitch/recordings/rlgstaterealty.velantro.net/archive/2021
-*****/mnt/wasabi/usr/local/freeswitch/recordings/jlpdispatching.velantro.net/archive/2020
-*****/mnt/wasabi/usr/local/freeswitch/recordings/judicialguardllp.velantro.net/archive/2020
-*****/mnt/wasabi/usr/local/freeswitch/recordings/juicywingzinc.velantro.net/archive/2020
-*****/mnt/wasabi/usr/local/freeswitch/recordings/justelitetransport.velantro.net/archive/2020
+****/mnt/wasabi/var/lib/freeswitch/recordings/beatwaycargo.velantro.net/archive/2021/Apr
+****/mnt/wasabi/var/lib/freeswitch/recordings/megadispatchincs.velantro.net/archive/2021/May
+****/mnt/wasabi/var/lib/freeswitch/recordings/mgline.velantro.net/archive/2021/Apr
+****/mnt/wasabi/var/lib/freeswitch/recordings/mslogistics.velantro.net/archive/2021/Apr
+****/mnt/wasabi/var/lib/freeswitch/recordings/onelogisticsnetwork.velantro.net/archive/2021/May
+****/mnt/wasabi/var/lib/freeswitch/recordings/tsa.velantro.net/archive/2021/May
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Jan
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Feb
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Mar
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/May
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Jun
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Jul
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Sep
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Oct
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2020/Nov
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2021/Jan
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2021/Feb
+****/mnt/wasabi/usr/local/freeswitch/recordings/itftrucking.velantro.net/archive/2021/Mar
+****/mnt/wasabi/usr/local/freeswitch/recordings/jlpdispatching.velantro.net/archive/2021/Jan
+****/mnt/wasabi/usr/local/freeswitch/recordings/jlpdispatching.velantro.net/archive/2021/Feb
+****/mnt/wasabi/usr/local/freeswitch/recordings/jlpdispatching.velantro.net/archive/2021/Mar
+****/mnt/wasabi/usr/local/freeswitch/recordings/jnettrans.velantro.net/archive/2021/May
+****/mnt/wasabi/usr/local/freeswitch/recordings/judicialguardllp.velantro.net/archive/2021/Jan
+****/mnt/wasabi/usr/local/freeswitch/recordings/judicialguardllp.velantro.net/archive/2021/Feb
+****/mnt/wasabi/usr/local/freeswitch/recordings/judicialguardllp.velantro.net/archive/2021/Mar
+****/mnt/wasabi/usr/local/freeswitch/recordings/juicywingzinc.velantro.net/archive/2021/Jan
+****/mnt/wasabi/usr/local/freeswitch/recordings/juicywingzinc.velantro.net/archive/2021/Feb
+****/mnt/wasabi/usr/local/freeswitch/recordings/juicywingzinc.velantro.net/archive/2021/Mar
+****/mnt/wasabi/usr/local/freeswitch/recordings/justelitetransport.velantro.net/archive/2021/Jan
+****/mnt/wasabi/usr/local/freeswitch/recordings/justelitetransport.velantro.net/archive/2021/Feb
+****/mnt/wasabi/usr/local/freeswitch/recordings/justelitetransport.velantro.net/archive/2021/Mar
 };
 
 print "#!/bin/sh\n";
 for $e (@lines) {
-   ($p, $d, $y) = $e =~ m{\*\*\*\*\*/mnt/wasabi/(.+?)/freeswitch/recordings/(.+)/archive/(\d+)$};
-   $cmd = "cp -purfn /mnt/s3/$p/freeswitch/recordings/$d/archive/$y /mnt/wasabi/$p/freeswitch/recordings/$d/archive &";
+   ($p, $d, $y, $m) = $e =~ m{\*\*\*\*\*/mnt/wasabi/(.+?)/freeswitch/recordings/(.+)/archive/(\d+)/(\w+)$};
+   $cmd = "cp -purfn /mnt/s3/$p/freeswitch/recordings/$d/archive/$y/$m /mnt/wasabi/$p/freeswitch/recordings/$d/archive/$y &";
    print $cmd, "\n";
 }
