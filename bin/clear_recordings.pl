f0f25ecdc79cc7a8025497d3b4fa321cc7673a60
salzh <zhongxiang721@gmail.com>
2021-05-09 11:19:08 +0800

    add script to clear recordings on s3 already moved to wasabi

--- bin/clear_recordings.pl
+++ bin/clear_recordings.pl
@@ -4,7 +4,7 @@ for $b (@base_dir) {
    while (</mnt/s3/$b/recordings/*>) {
       #print $_, "\n";
       ($domain) = $_ =~ m{recordings/(.+)$};
-      print $domain, "...\n";
+      #print $domain, "...\n";
       $cmd = "find $_/archive -name \".wav\"";
       print "$cmd > /var/log/recordings_$domain.list &\n";      
    }   
