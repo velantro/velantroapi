3bd6c5414c7c6b9f2d1cd465b70311a0a8e14c24
salzh <zhongxiang721@gmail.com>
2021-05-09 11:38:23 +0800

    add script to clear recordings on s3 already moved to wasabi

--- bin/clear_recordings.pl
+++ bin/clear_recordings.pl
@@ -4,9 +4,44 @@ for $b (@base_dir) {
    while (</mnt/s3/$b/recordings/*>) {
       #print $_, "\n";
       ($domain) = $_ =~ m{recordings/(.+)$};
-      #print $domain, "...\n";
-      $cmd = "find $_/archive -name \".wav\"";
-      print "$cmd > /var/log/recordings_$domain.list &\n";      
+      #print $d, "\n";
+      
+      
+      
+      
+      for $y (2020 .. 2021) {
+         if ((!-d "/mnt/s3/$b/recordings/$domain/archive/$y")) {
+            warn "/mnt/s3/$b/recordings/$domain/archive/$y not found\n";
+            next;
+         }
+        
+         
+         if ($y eq '2021') {
+            @months = qw/Jan  Feb  Mar  Apr  May/;
+         } else {
+            @months = qw/Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov  Dec/;
+         }
+         
+         for $m(@months) {
+            if ((!-d "/mnt/s3/$b/recordings/$domain/archive/$y/$m")) {
+               warn "/mnt/s3/$b/recordings/$domain/archive/$y/$m not found\n";
+               next;
+            }
+            
+            for $d (1..31) {
+               $d = sprintf("%02d", $d);
+               if ((!-d "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d")) {
+                  warn "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d not found\n";
+                  next;
+               }
+               
+               for $filename (glob "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d/*") {
+                  next unless /\.(?:wav|mp3)/i;
+                  print "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d/$filename";
+               }
+            }
+         }
+      }
    }   
 }
 
