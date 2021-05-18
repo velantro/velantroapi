72ea5adac3ce32eb0673f0e10a83d8ca0c6ffb4f
salzh <zhongxiang721@gmail.com>
2021-05-18 10:19:03 +0800

    remove tenant if no 2021 directory found

--- bin/clear_recordings2021.pl
+++ bin/clear_recordings2021.pl
@@ -12,54 +12,9 @@ for $b (@base_dir) {
    while (</mnt/s3/$b/recordings/*>) {
       #print $_, "\n";
       ($domain) = $_ =~ m{recordings/(.+)$};
-      #print $d, "\n";
-      
-      
-      
-      
-      for $y (2020 .. 2021) {
-         if ((!-d "/mnt/s3/$b/recordings/$domain/archive/$y")) {
-            warn "/mnt/s3/$b/recordings/$domain/archive/$y not found\n";
-            next;
-         }
-        
-         
-         if ($y eq '2021') {
-            @months = qw/Jan  Feb  Mar  Apr  May/;
-         } else {
-            @months = qw/Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov  Dec/;
-         }
-         
-         for $m(@months) {
-            if ((!-d "/mnt/s3/$b/recordings/$domain/archive/$y/$m")) {
-               warn "/mnt/s3/$b/recordings/$domain/archive/$y/$m not found\n";
-               next;
-            }
-            
-            for $d (1..31) {
-               $d = sprintf("%02d", $d);
-               if ((!-d "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d")) {
-                  warn "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d not found\n";
-                  next;
-               }
-               
-               for $filename (glob "/mnt/s3/$b/recordings/$domain/archive/$y/$m/$d/*") {
-                  next unless $filename =~ m{/mnt/s3/(.+)\.(wav|mp3)}i;
-                  $wa_file = "/mnt/wasabi/$1.$2";
-                  if (!-e $wa_file) {
-                     print "copy $filename to $wa_file!\n";
-                     if (not -d "/mnt/wasabi/$b/recordings/$domain/archive/$y/$m/$d" ) {
-                        print "mdir \"/mnt/wasabi/$b/recordings/$domain/archive/$y/$m/$d\"\n";
-                        mkdir "/mnt/wasabi/$b/recordings/$domain/archive/$y/$m/$d";
-                     }                     
-                     copy($filename, $wa_file) or next;                                         
-                  }
-                  print "unlink $filename!\n";
-                  unlink $filename;                  
-               }
-            }
-         }
-      }
+      if (not -d "/mnt/s3/$b/recordings/$domain/archive/2021") {
+         print "$domain: " . "/mnt/s3/$b/recordings/$domain/archive/2021 not found!\n"; 
+      }      
    }   
 }
 
