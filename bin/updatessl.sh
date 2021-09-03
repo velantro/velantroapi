4a23420caa5c813bef26b1b171429ce46285d31d
salzh <zhongxiang721@gmail.com>
2021-09-03 14:07:04 +0800

    add script to auto update ssl for sms.velantro.net and manage.velantro.net from 134

--- bin/updatessl.sh
+++ bin/updatessl.sh
@@ -6,3 +6,12 @@ scp 134.velantro.net:/etc/dehydrated/certs/velantro.net/privkey.pem /tmp
 
 scp -oPort=2222 /tmp/fullchain.pem sms.velantro.net:/etc/pki/tls/certs/fullchain.pem
 scp -oPort=2222 /tmp/privkey.pem sms.velantro.net:/etc/pki/tls/private/privkey.pem
+ssh -p 2222 sms.velantro.net 'service httpd restart'
+
+scp /tmp/fullchain.pem manage.velantro.net:/etc/pki/tls/certs/
+scp /tmp/privkey.pem manage.velantro.net:/etc/pki/tls/private/
+
+ssh manage.velantro.net 'service httpd restart'
+
+
+
