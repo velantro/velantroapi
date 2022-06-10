e9e9e18d8feaef10869788171ec29deb94310043
salzh <zhongxiang721@gmail.com>
2022-06-10 20:24:42 +0800

    add updateflagsmtp

--- bin/updatesmtppass.sh
+++ bin/updatesmtppass.sh
@@ -3,6 +3,7 @@
 pass=FlagNot2017!
 
 echo "update v_default_settings set default_setting_value='$pass' where  default_setting_subcategory='smtp_password'" | psql fusionpbx -U fusionpbx -h 127.0.0.1;
+echo "select default_setting_value from v_default_settings where  default_setting_subcategory='smtp_password'" | psql fusionpbx -U fusionpbx -h 127.0.0.1;
 
 echo "update v_default_settings set default_setting_value='notification@flagmantelecom.com' where  default_setting_subcategory='smtp_from'" | psql fusionpbx -U fusionpbx -h 127.0.0.1;
 echo "update v_default_settings set default_setting_value='notification@flagmantelecom.com' where  default_setting_subcategory='fax_smtp_from'" | psql fusionpbx -U fusionpbx -h 127.0.0.1;
