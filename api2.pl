98fe36ac8dbdb764e0df29d47ea6863962f2d24a
salzh <zhongxiang721@163.com>
2024-04-05 07:11:56 +0800

    fix recording issue

--- api2.pl
+++ api2.pl
@@ -558,10 +558,14 @@ sub do_cdr() {
 		#$c++;
 		$recording_url = '';
 		$recording_filename = $row->{record_path} . '/' . $row->{record_name};
-		if (-e $recording_filename) {
-			$recording_url = "/app/recordings/recordings2.php?filename=" . encode_base64($recording_filename, '');
-			$record_size = -s $recording_filename;
+		if (!-e $recording_filename) {
+			@today = &get_today();
+			$recording_filename = "/var/lib/freeswitch/recordings/" . $row->{domain_name} . "/archive" . "/"  . $today[0] . "/" . $today[1] . "/" . $today[2] . "/" . $row->{xml_cdr_uuid} . ".mp3";
+			
 		}
+		warn "recording_filename: $recording_filename";
+		$recording_url = "/app/recordings/recordings2.php?filename=" . encode_base64($recording_filename, '');
+		$record_size = -s $recording_filename;
 		push @$list, {xml_cdr_uuid => $row->{xml_cdr_uuid}, domain_name => $row->{domain_name}, caller_id_number => $row->{caller_id_number},caller_id_name => $row->{caller_id_name},
 					  destination_number => $row->{destination_number}, did => $row->{caller_destination}, start_stamp => $row->{start_stamp}, end_stamp => $row->{end_stamp},
 					  billsec => $row->{billsec},duration => $row->{duration},reocrd_size => $record_size || 0,record_url => $recording_url};
