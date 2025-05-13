212106c6fa196028bf61a1f4baad821d0ea30668
salzh <zhongxiang721@163.com>
2025-05-13 15:04:23 +0800

    read client_id and client_secret for zoho from default settings

--- bin/zoho_daemon.pl
+++ bin/zoho_daemon.pl
@@ -745,14 +745,19 @@ S
 }
 
 sub refresh_zoho_tokens() {
+	%default = &database_select_as_hash("select default_setting_subcategory,default_setting_value from v_default_setttings where (default_setting_subcategory = 'zoho_client_id' or default_setting_subcategory = 'zoho_client_secret')", "value");
+	$client_id = $default{zoho_client_id}{value};
+	$client_secret = $default{zoho_client_secret}{value};
+	
 	%zoho_tokens = &database_select_as_hash("select ext,zohouser,refresh_token,access_token,extract(epoch from v_zoho_users.update_date),zoho_api_domain
 from v_zoho_users left join v_zoho_token on v_zoho_users.zoho_token_uuid=v_zoho_token.zoho_token_uuid
 where ext is not null", "zohouser,refresh_token,access_token,update_time,api_domain");
 	for $key(keys %zoho_tokens) {
 
 		if (time-$zoho_tokens{$key}{update_time} > 1800) {
-			$client_id = '1000.Z7AE3OOFNGE5Y3IJPZGJWK45QWTM0D';
-			$client_secret = '9d6bb961106e7e495928b13f6bec05e56c1cbba6dc';
+			#$client_id = '1000.Z7AE3OOFNGE5Y3IJPZGJWK45QWTM0D';
+			#$client_secret = '9d6bb961106e7e495928b13f6bec05e56c1cbba6dc';
+			warn "client_id=$client_id,client_server=$client_secret";
 			$refresh_token = $zoho_tokens{$key}{refresh_token};
 			$out = `curl -k 'https://accounts.zoho.com/oauth/v2/token' -X POST -d 'refresh_token=$refresh_token&client_id=$client_id&client_secret=$client_secret&grant_type=refresh_token'`;
 			%h = &Json2Hash($out);
